faf5bea9a444f1d50ef6a460e2001edfc3c8072d
young-jin-son <sdudwls128@naver.com>
2025-02-16 03:10:30 +0900

    feat: 주간 통계 페이지에서 날짜 클릭 시 해당 날짜의 일간 통계 페이지로 이동 #134

--- frontend/src/pages/WeekStatisticsPage/components/WeekStatisticsMain/WeekDayList/WeekDayItem/index.tsx
+++ frontend/src/pages/WeekStatisticsPage/components/WeekStatisticsMain/WeekDayList/WeekDayItem/index.tsx
@@ -1,3 +1,5 @@
+import { useNavigate } from 'react-router';
+
 import * as S from './WeekDayItem.styled';
 
 import Icon from '@/components/Icon';
@@ -14,14 +16,20 @@ interface WeekDayItemProps {
 }
 
 const WeekDayItem = ({ date, usageTime, isToday }: WeekDayItemProps) => {
+  const navigate = useNavigate();
   const today = new Date();
   const currentDate = new Date(date.year, date.month - 1, date.day);
-
   const isFutureDate = currentDate > today;
+
+  const handleClickDay = () => {
+    navigate(
+      `/statistics/day?date=${date.year}-${date.month.toString().padStart(2, '0')}-${date.day.toString().padStart(2, '0')}`,
+    );
+  };
 
   return (
     <S.WeekDayItemContainer>
-      <S.DayButton disabled={isFutureDate}>
+      <S.DayButton disabled={isFutureDate} onClick={handleClickDay}>
         {usageTime > 0 && <Icon icon="Fire" width={16} height={16} color={theme.colors.green200} />}
         <S.Day $isToday={isToday} $isFutureDay={isFutureDate}>
           {date.day}
