7bf249e6070895f5667082651df2778181bf1975
Gyuhan Park <rbgks1937@naver.com>
2025-02-16 00:30:14 +0900

    chore: pwa 화면 및 화면 방향 고정 설정 추가 #118

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -14,6 +14,10 @@ export default defineConfig({
         short_name: 'do-reburn',
         description: '통학생을 위한 이동 시간 할일 관리 서비스, 두리번',
         theme_color: '#ffffff',
+        background_color: '#ffffff',
+        display: 'standalone',
+        orientation: 'portrait',
+        start_url: '/',
         icons: [
           { src: '/icons/192x192.png', sizes: '192x192', type: 'image/png' },
           { src: '/icons/512x512.png', sizes: '512x512', type: 'image/png' },
