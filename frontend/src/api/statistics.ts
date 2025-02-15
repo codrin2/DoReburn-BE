fea82e17cf87c9dac47e872b5c3c7ee885fddca7
young-jin-son <sdudwls128@naver.com>
2025-02-16 03:01:16 +0900

    feat: 일간 통계 조회 API 함수 생성 #134

--- frontend/src/api/statistics.ts
+++ frontend/src/api/statistics.ts
@@ -22,9 +22,34 @@ interface WeekStatisticsResponse {
   data: WeekStatistics;
 }
 
+interface DayStatistics {
+  totalMoveTime: number;
+  totalUsageTime: number;
+  feedbacks: {
+    mood: string;
+    memo: string;
+  }[];
+  categoryTodoCounts: {
+    category: string;
+    count: number;
+  }[];
+}
+
+interface DayStatisticsResponse {
+  data: DayStatistics;
+}
+
 export const getWeekStatistics = async ({ startDate }: { startDate: string }) => {
   const result = await fetchClient.get<WeekStatisticsResponse>(
     `${API_URL.weekStatistics}?startDate=${startDate}`,
+  );
+
+  return result.data;
+};
+
+export const getDayStatistics = async ({ date }: { date: string }) => {
+  const result = await fetchClient.get<DayStatisticsResponse>(
+    `${API_URL.dayStatistics}?date=${date}`,
   );
 
   return result.data;
