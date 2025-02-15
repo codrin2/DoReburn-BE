58a5b4689850694cb932f4f3a1074c2c067390d4
codrin2 <hssang127@gmail.com>
2025-02-16 00:19:45 +0900

    feat: 사용자 위치 정보 저장 API 구현 #130

--- backend/src/main/java/com/dubu/backend/member/api/MemberController.java
+++ backend/src/main/java/com/dubu/backend/member/api/MemberController.java
@@ -1,7 +1,9 @@
 package com.dubu.backend.member.api;
 
+import com.dubu.backend.global.anotation.Polling;
 import com.dubu.backend.global.domain.SuccessResponse;
 import com.dubu.backend.member.application.MemberService;
+import com.dubu.backend.member.dto.MemberLocation;
 import com.dubu.backend.member.dto.request.MemberInfoUpdateRequest;
 import com.dubu.backend.member.dto.request.MemberOnboardingRequest;
 import com.dubu.backend.member.dto.request.MemberStatusUpdateRequest;
@@ -81,5 +83,15 @@ public class MemberController implements MemberApi {
             @RequestBody MemberStatusUpdateRequest request
     ) {
         memberService.updateMemberStatus(memberId, request.status());
+    }
+
+    @Polling
+    @ResponseStatus(NO_CONTENT)
+    @PutMapping("/location")
+    public void updateMemberLocation(
+            @RequestAttribute("memberId") Long memberId,
+            @RequestBody MemberLocation memberLocation
+    ) {
+        memberService.updateMemberLocation(memberId, memberLocation);
     }
 }
