befb8afc3082f82f4740646bbba02616e95c0c17
young-jin-son <sdudwls128@naver.com>
2025-02-15 18:16:18 +0900

    feat: 주간 통계 페이지 메인 컴포넌트 구현 #103

--- frontend/src/pages/WeekStatisticsPage/components/WeekStatisticsMain/WeekStatisticsMain.styled.ts
+++ frontend/src/pages/WeekStatisticsPage/components/WeekStatisticsMain/WeekStatisticsMain.styled.ts
@@ -1,8 +1,14 @@
 import styled from 'styled-components';
 
 export const WeekStatisticsMainContainer = styled.div`
-  width: 100%;
   display: flex;
   flex-direction: column;
   gap: 3.2rem;
+  overflow: hidden;
+`;
+
+export const WeekAchievementContainer = styled.div`
+  flex: 1;
+  overflow-y: auto;
+  padding-bottom: 3rem;
 `;
--- frontend/src/pages/WeekStatisticsPage/components/WeekStatisticsMain/index.tsx
+++ frontend/src/pages/WeekStatisticsPage/components/WeekStatisticsMain/index.tsx
@@ -1,19 +1,50 @@
-import WeekDate from './WeekDate';
+import { useEffect, useState } from 'react';
+
+import WeekCategoryList from './WeekCategoryList';
+import WeekDayList from './WeekDayList';
 import WeekOverview from './WeekOverview';
 import * as S from './WeekStatisticsMain.styled';
-import WeekTodos from './WeekTodos';
 
-interface WeekStatisticsMainProps {
-  weekStartDate: string;
-  setWeekStartDate: (date: string) => void;
-}
+import useWeekStatisticsQuery from '@/pages/WeekStatisticsPage/hooks/useWeekStatisticsQuery';
+import { getStartOfWeek } from '@/pages/WeekStatisticsPage/WeekStatisticsPage.utils';
 
-const WeekStatisticsMain = ({ weekStartDate, setWeekStartDate }: WeekStatisticsMainProps) => {
+const WeekStatisticsMain = () => {
+  const today = new Date().toISOString();
+  const [weekStartDate, setWeekStartDate] = useState(getStartOfWeek(today));
+
+  const { data: weekStatistics } = useWeekStatisticsQuery(weekStartDate);
+
+  useEffect(() => {
+    const searchParams = new URLSearchParams(window.location.search);
+    const startDate = searchParams.get('startDate');
+
+    if (startDate) {
+      setWeekStartDate(getStartOfWeek(startDate));
+    }
+  }, []);
+
+  useEffect(() => {
+    window.history.pushState({}, '', `/statistics/week?startDate=${weekStartDate}`);
+  }, [weekStartDate]);
+
+  const overviewProps = {
+    moveTime: weekStatistics?.totalMoveTime ?? 0,
+    usageTime: weekStatistics?.totalUsageTime ?? 0,
+    lastWeekDiff: weekStatistics?.lastWeekDiff ?? 0,
+    todoCount: weekStatistics?.totalTodoCount ?? 0,
+  };
+
   return (
     <S.WeekStatisticsMainContainer>
-      <WeekDate weekStartDate={weekStartDate} setWeekStartDate={setWeekStartDate} />
-      <WeekOverview />
-      <WeekTodos />
+      <WeekDayList
+        weekStartDate={weekStartDate}
+        setWeekStartDate={setWeekStartDate}
+        dayUsageTime={weekStatistics?.dayUsageTimes ?? []}
+      />
+      <S.WeekAchievementContainer>
+        <WeekOverview {...overviewProps} />
+        <WeekCategoryList categoryRanking={weekStatistics?.categoryTodoCounts ?? []} />
+      </S.WeekAchievementContainer>
     </S.WeekStatisticsMainContainer>
   );
 };
