4242b19429237f1479197f4d777f87bd20e40bb2
young-jin-son <sdudwls128@naver.com>
2025-02-15 18:21:09 +0900

    feat: 주간 통계 페이지 날짜 아이템 컴포넌트 구현 #103

---
+++ frontend/src/pages/WeekStatisticsPage/components/WeekStatisticsMain/WeekDayList/WeekDayItem/WeekDayItem.styled.ts
@@ -0,0 +1,41 @@
+import styled from 'styled-components';
+
+export const WeekDayItemContainer = styled.li`
+  width: 3.8rem;
+`;
+
+export const DayButton = styled.button`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+
+  &:disabled {
+    cursor: default;
+  }
+`;
+
+export const Day = styled.div<{ $isToday: boolean; $isFutureDay: boolean }>`
+  width: 3.6rem;
+  height: 3.6rem;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  ${({ theme }) => theme.fonts.body15Med};
+  color: ${({ theme, $isToday, $isFutureDay }) => {
+    if ($isToday) {
+      return theme.colors.green700;
+    } else if ($isFutureDay) {
+      return theme.colors.gray400;
+    } else {
+      return theme.colors.gray800;
+    }
+  }};
+  background-color: ${({ $isToday, theme }) => ($isToday ? theme.colors.green50 : 'transparent')};
+  border-radius: ${({ $isToday }) => ($isToday ? '50%' : '0')};
+`;
+
+export const DayUsageTime = styled.div`
+  ${({ theme }) => theme.fonts.caption12Reg};
+  color: ${({ theme }) => theme.colors.gray400};
+`;
