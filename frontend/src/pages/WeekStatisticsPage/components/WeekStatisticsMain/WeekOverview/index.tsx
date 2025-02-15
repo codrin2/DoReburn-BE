a46bce696a7c340b174ef3ebde7cf76afe29e006
young-jin-son <sdudwls128@naver.com>
2025-02-15 18:22:14 +0900

    refactor: 주간 통계 개요 컴포넌트에 동적 데이터 적용 #103

--- frontend/src/pages/WeekStatisticsPage/components/WeekStatisticsMain/WeekOverview/index.tsx
+++ frontend/src/pages/WeekStatisticsPage/components/WeekStatisticsMain/WeekOverview/index.tsx
@@ -1,29 +1,39 @@
 import * as S from './WeekOverview.styled';
 
-const WeekOverview = () => {
+interface WeekOverviewProps {
+  moveTime: number;
+  usageTime: number;
+  lastWeekDiff: number;
+  todoCount: number;
+}
+
+const WeekOverview = ({ moveTime, usageTime, lastWeekDiff, todoCount }: WeekOverviewProps) => {
   return (
     <S.WeekOverviewContainer>
       <S.WeekOverviewTitle>주간 현황</S.WeekOverviewTitle>
+
       <S.WeekOverviewContent>
         <S.WeekAchievement>
-          <span>425분 동안</span>
-          <span>20개의 할 일을 했어요</span>
+          <span>{usageTime}분 동안</span>
+          <span>{todoCount}개의 할 일을 했어요</span>
         </S.WeekAchievement>
 
         <S.CompareLastWeek>
           <S.CompareTitle>지난 주와의 비교</S.CompareTitle>
-          <S.CompareValue>+ 47분</S.CompareValue>
+          <S.CompareValue>
+            {lastWeekDiff >= 0 ? '+' : '-'} {lastWeekDiff}분
+          </S.CompareValue>
         </S.CompareLastWeek>
 
         <S.TimeInfoWrapper>
           <S.TimeInfoBox>
             <S.TimeInfoLabel>총 이동 시간</S.TimeInfoLabel>
-            <S.TimeInfoValue>520분</S.TimeInfoValue>
+            <S.TimeInfoValue>{moveTime}분</S.TimeInfoValue>
           </S.TimeInfoBox>
           <S.Divider />
           <S.TimeInfoBox>
             <S.TimeInfoLabel>총 활용 시간</S.TimeInfoLabel>
-            <S.TimeInfoValue>425분</S.TimeInfoValue>
+            <S.TimeInfoValue>{usageTime}분</S.TimeInfoValue>
           </S.TimeInfoBox>
         </S.TimeInfoWrapper>
       </S.WeekOverviewContent>
