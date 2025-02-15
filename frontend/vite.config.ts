399e321bff9e9693924ccf84fd8731baad4acd01
Gyuhan Park <rbgks1937@naver.com>
2025-02-16 04:00:40 +0900

    chore: vitest 테스트 환경 세팅 #137

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,10 +1,15 @@
 import react from '@vitejs/plugin-react';
-import { defineConfig } from 'vite';
+import { defineConfig } from 'vitest/config';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: [{ find: '@', replacement: '/src' }],
+  },
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    setupFiles: './src/tests/setup.ts',
   },
 });
