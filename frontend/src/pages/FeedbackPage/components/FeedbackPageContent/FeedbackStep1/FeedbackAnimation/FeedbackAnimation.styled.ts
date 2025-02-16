08ea33f31f4aff835765d479885f1fad81f1f420
young-jin-son <sdudwls128@naver.com>
2025-02-16 14:30:49 +0900

    design: 피드백 애니메이션 크기 수정 #143

--- frontend/src/pages/FeedbackPage/components/FeedbackPageContent/FeedbackStep1/FeedbackAnimation/FeedbackAnimation.styled.ts
+++ frontend/src/pages/FeedbackPage/components/FeedbackPageContent/FeedbackStep1/FeedbackAnimation/FeedbackAnimation.styled.ts
@@ -5,8 +5,8 @@ export const AnimationContainer = styled.div`
   display: flex;
   justify-content: center;
   align-items: center;
-  width: 28rem;
-  height: 28rem;
+  width: 24rem;
+  height: 24rem;
   border-radius: 5rem;
   overflow: hidden;
   box-shadow: 0 0 2rem 0 rgba(0, 0, 0, 0.1);
