265e79cf72efa5a40446827c3463a054745d7a34
young-jin-son <sdudwls128@naver.com>
2025-02-16 12:48:51 +0900

    design: 피드백 애니메이션 스타일 수정 #143

--- frontend/src/pages/FeedbackPage/components/FeedbackPageContent/FeedbackStep1/FeedbackAnimation/FeedbackAnimation.styled.ts
+++ frontend/src/pages/FeedbackPage/components/FeedbackPageContent/FeedbackStep1/FeedbackAnimation/FeedbackAnimation.styled.ts
@@ -5,4 +5,9 @@ export const AnimationContainer = styled.div`
   display: flex;
   justify-content: center;
   align-items: center;
+  width: 28rem;
+  height: 28rem;
+  border-radius: 5rem;
+  overflow: hidden;
+  box-shadow: 0 0 2rem 0 rgba(0, 0, 0, 0.1);
 `;
