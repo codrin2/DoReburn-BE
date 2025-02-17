027b4e1e16cbc10318434c983caaf0b9b1f29a73
young-jin-son <sdudwls128@naver.com>
2025-02-17 19:07:00 +0900

    feat: 회원 정보조회 MSW 핸들러 추가 #144

--- frontend/src/mocks/handlers/member.ts
+++ frontend/src/mocks/handlers/member.ts
@@ -1,6 +1,7 @@
 import { http, HttpResponse } from 'msw';
 
 import MEMBER_ADDRESS from '../data/memberAddress.json';
+import MEMBER_INFO from '../data/memberInfo.json';
 
 import { MOCK_API_URL } from '@/constants/url';
 
@@ -12,7 +13,12 @@ const updateMemberStatusHandler = () => {
   return new HttpResponse(null, { status: 204 });
 };
 
+const getMemberInfoHandler = () => {
+  return HttpResponse.json(MEMBER_INFO);
+};
+
 export const handlers = [
   http.get(MOCK_API_URL.memberAddress, getMemberAddressHandler),
   http.patch(MOCK_API_URL.memberStatus, updateMemberStatusHandler),
+  http.get(MOCK_API_URL.memberInfo, getMemberInfoHandler),
 ];
