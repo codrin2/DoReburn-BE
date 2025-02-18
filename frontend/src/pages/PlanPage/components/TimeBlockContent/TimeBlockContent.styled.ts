8687cb8741064085e46c6f9d2aafa897cec98558
Gyuhan Park <rbgks1937@naver.com>
2025-02-18 22:55:23 +0900

    refactor: 지하철 호선에 따른 라인 색상 변경 #171

--- frontend/src/pages/PlanPage/components/TimeBlockContent/TimeBlockContent.styled.ts
+++ frontend/src/pages/PlanPage/components/TimeBlockContent/TimeBlockContent.styled.ts
@@ -15,13 +15,13 @@ export const TransportBarWrapper = styled.div`
   min-height: 21.1rem;
 `;
 
-export const TransportBar = styled.div<{ $trafficType: 'Subway' | 'Bus' }>`
+export const TransportBar = styled.div<{ $pathColor: string }>`
   display: flex;
   align-items: center;
 
   width: 0.4rem;
   height: 100%;
-  background-color: ${({ theme, $trafficType }) => theme.colors[$trafficType]};
+  background-color: ${({ $pathColor }) => $pathColor};
 
   border-radius: 0 0 0.4rem 0.4rem;
 `;
--- frontend/src/pages/PlanPage/components/TimeBlockHeader/index.tsx
+++ frontend/src/pages/PlanPage/components/TimeBlockHeader/index.tsx
@@ -2,6 +2,7 @@ import * as S from './TimeBlockHeader.styled';
 import { TRAFFIC_ICON, TRAFFIC_TYPE } from '../../PlanPage.constants';
 
 import Icon from '@/components/Icon';
+import { SUBWAY_LINES } from '@/constants/subwayLines';
 
 interface TimeBlockHeaderProps {
   trafficType: 'SUBWAY' | 'BUS';
@@ -9,11 +10,17 @@ interface TimeBlockHeaderProps {
 }
 
 const TimeBlockHeader = ({ trafficType, subwayCode }: TimeBlockHeaderProps) => {
+  const isSubwayColor = subwayCode && subwayCode in SUBWAY_LINES;
+
   return (
     <S.TransportHeader>
       <S.TransportIconWrapper $trafficType={TRAFFIC_ICON[trafficType]}>
         <Icon icon={TRAFFIC_ICON[trafficType]} width={16} height={16} />
-        {subwayCode && <S.SubwayNumber>{subwayCode}</S.SubwayNumber>}
+        {isSubwayColor && (
+          <S.SubwayNumber>
+            {SUBWAY_LINES[subwayCode as keyof typeof SUBWAY_LINES].name[0]}
+          </S.SubwayNumber>
+        )}
       </S.TransportIconWrapper>
       <S.TransportType $trafficType={TRAFFIC_ICON[trafficType]}>
         {TRAFFIC_TYPE[trafficType]}
