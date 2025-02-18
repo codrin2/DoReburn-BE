85ba0e2e3be269ca09878990c47252e4b075aac9
codrin2 <hssang127@gmail.com>
2025-02-18 20:37:55 +0900

    feat: ConflictException 409 예외 추가 #168

--- backend/src/main/java/com/dubu/backend/global/exception/InternalServerException.java
+++ backend/src/main/java/com/dubu/backend/global/exception/InternalServerException.java
@@ -1,7 +1,6 @@
 package com.dubu.backend.global.exception;
 
 public abstract class InternalServerException extends RuntimeException {
-
   public InternalServerException(String message) {
     super(message);
   }
