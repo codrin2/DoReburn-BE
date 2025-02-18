ff4ed6dc561ab985ddab7ef941855fad587506f9
Gyuhan Park <rbgks1937@naver.com>
2025-02-18 11:17:35 +0900

    chore: 환경 변수 타입 설정 #164

--- frontend/src/vite-env.d.ts
+++ frontend/src/vite-env.d.ts
@@ -1,1 +1,12 @@
 /// <reference types="vite/client" />
+
+interface ImportMetaEnv {
+  readonly VITE_BASE_URL: string;
+  readonly VITE_KAKAO_APP_KEY: string;
+  readonly VITE_KAKAO_REDIRECT_URI: string;
+  readonly VITE_KAKAO_CLIENT_SECRET: string;
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
