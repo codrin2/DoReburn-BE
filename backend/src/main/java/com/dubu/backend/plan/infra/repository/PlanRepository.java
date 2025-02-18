58ea40f4fd94d1f228cac1f7930b2cec60fa17e9
khw7385 <khw7385@naver.com>
2025-02-18 14:51:25 +0900

    feat: path join fetch 하여 Plan 조회하는 쿼리 메소드 및 pathIds 조회하는 쿼리 메소드 추가 #162

--- backend/src/main/java/com/dubu/backend/plan/infra/repository/PlanRepository.java
+++ backend/src/main/java/com/dubu/backend/plan/infra/repository/PlanRepository.java
@@ -16,4 +16,11 @@ public interface PlanRepository extends JpaRepository<Plan, Long> {
 
     @Query("SELECT p FROM Plan p JOIN FETCH p.feedback WHERE p.member = :member AND p.createdAt between :startTime AND :endTime")
     List<Plan> findByMemberAndCreatedAtBetween(Member member, LocalDateTime startTime, LocalDateTime endTime);
+
+    @Query("SELECT p FROM Plan p JOIN FETCH p.feedback JOIN FETCH p.paths WHERE p.member = :member AND p.createdAt between :startTime AND :endTime")
+    List<Plan> findWithPathsByMemberAndCreatedAtBetween(Member member, LocalDateTime startTime, LocalDateTime endTime);
+
+    // 통계 기능 최적화 작업을 위해 남겨둠
+    @Query("SELECT pa.id FROM Plan p JOIN p.paths pa WHERE p.member = :member AND p.createdAt between :startTime AND :endTime")
+    List<Long> findPathIdsWithPathsByMemberAndCreatedAtBetween(Member member, LocalDateTime startTime, LocalDateTime endTime);
 }
