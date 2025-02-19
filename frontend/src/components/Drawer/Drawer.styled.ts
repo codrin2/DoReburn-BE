79dfb5b8f9892bb1a550ef335467caae6f06282b
Gyuhan Park <rbgks1937@naver.com>
2025-02-19 19:10:21 +0900

    fix: drawer가 페이지에 맞게 위치하도록 fixed에서 absolute 설정 #170

--- frontend/src/components/Drawer/Drawer.styled.ts
+++ frontend/src/components/Drawer/Drawer.styled.ts
@@ -1,7 +1,7 @@
 import styled from 'styled-components';
 
 export const DrawerLayout = styled.div<{ $isOpen: boolean }>`
-  position: fixed;
+  position: absolute;
   top: 0;
   bottom: 0;
   right: 50%;
