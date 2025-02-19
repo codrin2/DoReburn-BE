9fe123a21480b93a71a1bad757482a300d4de0ea
codrin2 <hssang127@gmail.com>
2025-02-19 15:42:58 +0900

    feat: 리마인드 알림 전송시 이미 종료된 plan인지 체크하는 로직 추가 #173

--- backend/src/main/java/com/dubu/backend/notification/application/NotificationService.java
+++ backend/src/main/java/com/dubu/backend/notification/application/NotificationService.java
@@ -11,6 +11,9 @@ import com.dubu.backend.notification.dto.PushSubscriptionDto;
 import com.dubu.backend.notification.exception.DuplicateSubscriptionException;
 import com.dubu.backend.notification.exception.UnavailablePushServiceException;
 import com.dubu.backend.notification.infra.repository.PushSubscriptionRepository;
+import com.dubu.backend.plan.domain.Plan;
+import com.dubu.backend.plan.exception.PlanNotFoundException;
+import com.dubu.backend.plan.infra.repository.PlanRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import nl.martijndwars.webpush.Notification;
@@ -30,6 +33,7 @@ import java.util.List;
 public class NotificationService {
     private final VapidKeyConfig vapidKeyConfig;
     private final MemberRepository memberRepository;
+    private final PlanRepository planRepository;
     private final PushSubscriptionRepository subscriptionRepository;
 
     @Value("${admin.email}")
@@ -51,6 +55,13 @@ public class NotificationService {
     }
 
     public void sendPushNotification(PushMessageDto message) {
+        Plan currentPlan = planRepository.findById(message.planId())
+                .orElseThrow(() -> new PlanNotFoundException(message.planId()));
+
+        if (currentPlan.isCompleted()) {
+            return;
+        }
+
         List<PushSubscription> subscriptions = subscriptionRepository.findByMemberId((message.memberId()));
         PushService pushService;
 
@@ -65,7 +76,8 @@ public class NotificationService {
                 String payload = """
                 {
                     "title": "%s",
-                    "body": "%s"
+                    "body": "%s",
+                    "url": "https://do-reburn.site/plan"
                 }
                 """.formatted(message.title(), message.body());
 
