98a8756e61e7c1733a1b9764be9f409526a6c7c3
Gyuhan Park <rbgks1937@naver.com>
2025-02-19 17:24:28 +0900

    design: drawer 반응형 적용 #170

--- frontend/src/components/Drawer/Drawer.styled.ts
+++ frontend/src/components/Drawer/Drawer.styled.ts
@@ -6,7 +6,7 @@ export const DrawerLayout = styled.div<{ $isOpen: boolean }>`
   bottom: 0;
   right: 50%;
   transform: translateX(50%);
-  width: 37.5rem;
+  width: 100%;
   overflow: hidden;
 
   height: 100vh;
@@ -32,7 +32,7 @@ export const Content = styled.div<{ $isOpen: boolean }>`
   top: 0;
   right: 0;
 
-  width: 22.5rem;
+  width: 50%;
   height: 100%;
   padding: 2rem 2.8rem;
 
