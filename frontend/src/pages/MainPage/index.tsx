0a82be1a61a959083ba173cb6d912d74edd4032d
son-young-jin <sdudwls128@naver.com>
2025-02-20 11:13:13 +0900

    feat: 메인 화면에서 알림 권한 요청 기능 추가 #160

--- frontend/src/pages/MainPage/index.tsx
+++ frontend/src/pages/MainPage/index.tsx
@@ -8,11 +8,14 @@ import * as S from './MainPage.styled';
 
 import Header from '@/components/Header';
 import SearchAddress from '@/components/SearchAddress';
+import useNotificationPermission from '@/hooks/useNotificationPermission';
 import useQueryParamsDate from '@/hooks/useQueryParamsDate';
 import useRedirectByMemberStatus from '@/hooks/useRedirectByMemberStatus';
 
 const MainPage = () => {
   useRedirectByMemberStatus();
+  useNotificationPermission();
+
   const { isToday } = useQueryParamsDate();
   const [isSwitchAddress, toggle] = useReducer((prev) => !prev, false);
 
