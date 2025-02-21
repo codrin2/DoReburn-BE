3be7466818894dca82632430d0d11e811ef176da
Gyuhan Park <rbgks1937@naver.com>
2025-02-21 12:19:30 +0900

    chore: Notification 방어 코드 추가 #193

--- frontend/src/hooks/useNotificationPermission.ts
+++ frontend/src/hooks/useNotificationPermission.ts
@@ -9,7 +9,10 @@ const useNotificationPermission = () => {
     if (permission === 'default') {
       alert('이동 시간이 끝나기 전에 알림으로 리마인드를 받아보세요! 🚀\n 놓치지 않게 도와드려요.');
 
-      if ('Notification' in window || typeof Notification !== 'undefined') {
+      const isPWA = window.matchMedia('(display-mode: standalone)').matches;
+      const isIOS = /iPad|iPhone|iPod/.test(navigator.userAgent);
+
+      if ('Notification' in window && (!isIOS || isPWA)) {
         Notification.requestPermission().then((newPermission) => {
           setPermission(newPermission);
 
