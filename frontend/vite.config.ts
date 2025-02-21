3b0492c7cef1a585f5f355aa74efd1ff41a21634
Gyuhan Park <rbgks1937@naver.com>
2025-02-21 11:44:20 +0900

    fix: 불필요한 설정 파일 제거 #193

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -7,12 +7,6 @@ export default defineConfig(({ mode }) => ({
   plugins: [
     react(),
     VitePWA({
-      strategies: 'injectManifest',
-      filename: 'service-worker.js',
-      injectManifest: {
-        swSrc: '/service-worker.js',
-        maximumFileSizeToCacheInBytes: 5 * 1024 * 1024,
-      },
       includeAssets: ['favicon.ico', 'apple-touch-icon'],
       manifest: {
         name: '통학생을 위한 이동 시간 할일 관리 서비스, 두리번',
