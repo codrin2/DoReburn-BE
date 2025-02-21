4598c62cbd56fa5addc22f96fdff160c4eeacfe9
Gyuhan Park <rbgks1937@naver.com>
2025-02-21 11:25:25 +0900

    fix: IOS에서 Notification API를 불러오지 못하는 문제 해결 #193

--- frontend/src/hooks/useNotificationPermission.ts
+++ frontend/src/hooks/useNotificationPermission.ts
@@ -8,13 +8,16 @@ const useNotificationPermission = () => {
   useEffect(() => {
     if (permission === 'default') {
       alert('이동 시간이 끝나기 전에 알림으로 리마인드를 받아보세요! 🚀\n 놓치지 않게 도와드려요.');
-      Notification.requestPermission().then((newPermission) => {
-        setPermission(newPermission);
 
-        if (newPermission === 'granted') {
-          subscribePushNotification();
-        }
-      });
+      if ('Notification' in window) {
+        Notification.requestPermission().then((newPermission) => {
+          setPermission(newPermission);
+
+          if (newPermission === 'granted') {
+            subscribePushNotification();
+          }
+        });
+      }
     }
   }, [permission]);
 
