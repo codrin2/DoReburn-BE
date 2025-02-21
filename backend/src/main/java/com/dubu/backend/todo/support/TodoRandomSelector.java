4eb677f5bd622f2072d3c2c689ec0e56bfd8f6bd
codrin2 <hssang127@gmail.com>
2025-02-21 22:31:19 +0900

    feat(TodoRandomSelector): 추천 할 일 5개 이하인 경우 예외 적용 #181

--- backend/src/main/java/com/dubu/backend/todo/support/TodoRandomSelector.java
+++ backend/src/main/java/com/dubu/backend/todo/support/TodoRandomSelector.java
@@ -1,20 +1,23 @@
 package com.dubu.backend.todo.support;
 
+import com.dubu.backend.todo.exception.NotEnoughRecommendedTodosException;
 import org.springframework.stereotype.Component;
 
 import java.util.Collections;
 import java.util.List;
 import java.util.concurrent.ThreadLocalRandom;
 
-
 @Component
 public class TodoRandomSelector {
 
-    public Long selectOne(List<Long> todoIds){
+    public Long selectOne(List<Long> todoIds) {
         return todoIds.get(ThreadLocalRandom.current().nextInt(todoIds.size()));
     }
 
-    public List<Long> selectTodos(int num, List<Long> todoIds){
+    public List<Long> selectTodos(int num, List<Long> todoIds) {
+        if (num > todoIds.size()) {
+            throw new NotEnoughRecommendedTodosException();
+        }
         Collections.shuffle(todoIds);
         return todoIds.subList(0, num);
     }
