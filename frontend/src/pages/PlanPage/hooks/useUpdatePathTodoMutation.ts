2b6d8b65ccf7d1809d02cbbd7c5bd2d3fb6bb8e2
Gyuhan Park <rbgks1937@naver.com>
2025-02-22 17:47:53 +0900

    refactor: 드래그 앤 드롭을 경로별 할 일을 옮겼을 때 setQueryData로 쿼리에 즉각적으로 반영 #182

--- frontend/src/pages/PlanPage/hooks/useUpdatePathTodoMutation.ts
+++ frontend/src/pages/PlanPage/hooks/useUpdatePathTodoMutation.ts
@@ -1,7 +1,34 @@
 import { useMutation, useQueryClient } from '@tanstack/react-query';
 
+import { Path, PathTodo, PlanInfoResponse } from '@/api/plan';
 import { updatePathTodo } from '@/api/todo';
 import { QUERY_KEY } from '@/constants/queryKey';
+
+const findTodoAndPath = (paths: Path[], todoId: number) => {
+  for (const path of paths) {
+    const foundTodo = path.todos.find((todo) => todo.todoId === todoId);
+
+    if (foundTodo) {
+      return { oldPath: path, oldTodo: foundTodo };
+    }
+  }
+
+  return { oldPath: null, oldTodo: null };
+};
+
+const filterTodo = (paths: Path[], oldPathId: number, todoId: number) => {
+  return paths.map((path) =>
+    path.pathId === oldPathId
+      ? { ...path, todos: path.todos.filter((todo) => todo.todoId !== todoId) }
+      : path,
+  );
+};
+
+const addTodo = (paths: Path[], newPathId: number, todo: PathTodo) => {
+  return paths.map((path) =>
+    path.pathId === newPathId ? { ...path, todos: [...path.todos, todo] } : path,
+  );
+};
 
 const useUpdatePathTodoMutation = () => {
   const queryClient = useQueryClient();
@@ -9,8 +36,20 @@ const useUpdatePathTodoMutation = () => {
   return useMutation({
     mutationFn: ({ todoId, newPathId }: { todoId: number; newPathId: number }) =>
       updatePathTodo({ todoId, newPathId }),
-    onSuccess: () => {
-      queryClient.invalidateQueries({ queryKey: [QUERY_KEY.planInfo] });
+    onSuccess: (_, { todoId, newPathId }) => {
+      queryClient.setQueryData<PlanInfoResponse['data']>([QUERY_KEY.planInfo], (oldData) => {
+        if (!oldData) return oldData;
+
+        const { oldPath, oldTodo } = findTodoAndPath(oldData.paths, todoId);
+
+        if (!oldPath || !oldTodo) return oldData;
+
+        const updatedPaths = filterTodo(oldData.paths, oldPath.pathId, todoId);
+        const finalPaths = addTodo(updatedPaths, newPathId, oldTodo);
+
+        return { ...oldData, paths: finalPaths };
+      });
+
       queryClient.invalidateQueries({ queryKey: [QUERY_KEY.recommendLimit] });
       queryClient.invalidateQueries({ queryKey: [QUERY_KEY.recommendAll] });
       queryClient.invalidateQueries({ queryKey: [QUERY_KEY.routeTodoList] });
