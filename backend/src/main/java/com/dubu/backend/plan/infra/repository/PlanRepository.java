b9f890588d0d2a650d9f9b73135e2d7a306f69e7
khw7385 <khw7385@naver.com>
2025-02-22 16:38:56 +0900

    fix: 해당 날짜의 계획을 찾는 쿼리 메소드에서 필터링 조건 추가 #205 isCompleted = true 인 경우만

--- backend/src/main/java/com/dubu/backend/plan/infra/repository/PlanRepository.java
+++ backend/src/main/java/com/dubu/backend/plan/infra/repository/PlanRepository.java
@@ -14,10 +14,10 @@ public interface PlanRepository extends JpaRepository<Plan, Long> {
     @Query("SELECT p FROM Plan p LEFT JOIN FETCH p.feedback WHERE p.member.id = :memberId ORDER BY p.createdAt DESC LIMIT 1")
     Optional<Plan> findTopByMemberIdOrderByCreatedAtDesc(Long memberId);
 
-    @Query("SELECT p FROM Plan p JOIN FETCH p.feedback WHERE p.member = :member AND p.createdAt between :startTime AND :endTime")
+    @Query("SELECT p FROM Plan p JOIN FETCH p.feedback WHERE p.member = :member AND p.isCompleted = true AND p.createdAt between :startTime AND :endTime")
     List<Plan> findByMemberAndCreatedAtBetween(Member member, LocalDateTime startTime, LocalDateTime endTime);
 
-    @Query("SELECT p FROM Plan p JOIN FETCH p.feedback JOIN FETCH p.paths WHERE p.member = :member AND p.createdAt between :startTime AND :endTime")
+    @Query("SELECT p FROM Plan p JOIN FETCH p.feedback JOIN FETCH p.paths WHERE p.member = :member AND p.isCompleted = true AND p.createdAt between :startTime AND :endTime")
     List<Plan> findWithPathsByMemberAndCreatedAtBetween(Member member, LocalDateTime startTime, LocalDateTime endTime);
 
     // 통계 기능 최적화 작업을 위해 남겨둠
