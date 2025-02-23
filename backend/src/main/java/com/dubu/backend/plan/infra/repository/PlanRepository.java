09aaf89113914638f9b6223627903495a26ce524
khw7385 <khw7385@naver.com>
2025-02-24 01:33:18 +0900

    feat: 회원과 종료인지 여부로 필터링하는 최근 계획 조회 쿼리 메서드 추가 #199

--- backend/src/main/java/com/dubu/backend/plan/infra/repository/PlanRepository.java
+++ backend/src/main/java/com/dubu/backend/plan/infra/repository/PlanRepository.java
@@ -13,7 +13,10 @@ public interface PlanRepository extends JpaRepository<Plan, Long> {
     @Query("SELECT p FROM Plan p LEFT JOIN FETCH p.feedback WHERE p.member.id = :memberId ORDER BY p.createdAt DESC LIMIT 1")
     Optional<Plan> findTopByMemberIdOrderByCreatedAtDesc(Long memberId);
 
-    @Query("SELECT p FROM Plan p JOIN FETCH p.feedback WHERE p.member = :member AND p.isCompleted = true AND p.createdAt between :startTime AND :endTime")
+    @Query("SELECT p FROM Plan p LEFT JOIN FETCH p.feedback WHERE p.member= :member AND p.isCompleted = :isCompleted ORDER BY p.createdAt DESC LIMIT 1")
+    Optional<Plan> findTopByMemberAndIsCompletedOrderByCreatedAtDesc(Member member, boolean isCompleted);
+
+    @Query("SELECT p FROM Plan p JOIN FETCH p.feedback WHERE p.member = :member AND p.createdAt between :startTime AND :endTime")
     List<Plan> findByMemberAndCreatedAtBetween(Member member, LocalDateTime startTime, LocalDateTime endTime);
 
     @Query("SELECT p FROM Plan p JOIN FETCH p.feedback JOIN FETCH p.paths WHERE p.member = :member AND p.isCompleted = true AND p.createdAt between :startTime AND :endTime")
