9bb6ebbced204eae1ed6454619d8bb0d484470bd
Gyuhan Park <rbgks1937@naver.com>
2025-02-25 00:32:41 +0900

    fix: 드래그한 요소가 있을 때만 touchEnd에서 클릭 이벤트 막기 #227

--- frontend/src/pages/PlanPage/components/TimeBlockContent/PlanContent/index.tsx
+++ frontend/src/pages/PlanPage/components/TimeBlockContent/PlanContent/index.tsx
@@ -76,13 +76,13 @@ const PlanContent = ({ paths }: PlanContentProps) => {
   };
 
   const handleTouchEnd = async (e: React.TouchEvent<HTMLElement>) => {
-    e.preventDefault();
-
     if (longPressTimeoutRef.current) {
       clearTimeout(longPressTimeoutRef.current);
     }
 
     if (!draggingTodo) return;
+
+    e.preventDefault();
 
     const touch = e.changedTouches[0];
 
