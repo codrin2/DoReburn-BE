ccf41d95d0738905212faabc37bfcd8af08ff892
Gyuhan Park <rbgks1937@naver.com>
2025-02-24 17:39:16 +0900

    fix: 토스트 메세지 수정 #227

--- frontend/src/constants/message.ts
+++ frontend/src/constants/message.ts
@@ -1,3 +1,5 @@
+import { MAX_TODO_ITEM_LENGTH } from './config';
+
 import { TodoType } from '@/types/todo';
 
 const DATE_TEXT = {
@@ -11,7 +13,7 @@ export const TODO_TOAST_MESSAGE = {
   add: (todoType: TodoType) => `${DATE_TEXT[todoType]} 할 일에 추가되었어요`,
   delete: (dateType: 'TODAY' | 'TOMORROW') => `${DATE_TEXT[dateType]} 할 일에서 삭제되었어요`,
   limit: (dateType: 'TODAY' | 'TOMORROW') =>
-    `${DATE_TEXT[dateType]} 할 일은 최대 3개까지 추가할 수 있어요`,
+    `${DATE_TEXT[dateType]} 할 일은 최대 ${MAX_TODO_ITEM_LENGTH}개까지 추가할 수 있어요`,
   addFavorite: '즐겨찾기에 추가되었어요',
   deleteFavorite: '즐겨찾기에서 삭제되었어요',
 };
