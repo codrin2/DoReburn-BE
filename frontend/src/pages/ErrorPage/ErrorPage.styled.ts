fed7c7cbdbf17720e6d5bf9c29521d4931d3c64c
Gyuhan Park <rbgks1937@naver.com>
2025-02-25 07:09:29 +0900

    feat: 경로 오류 및 에러 페이지 구현 #237

---
+++ frontend/src/pages/ErrorPage/ErrorPage.styled.ts
@@ -0,0 +1,40 @@
+import styled from 'styled-components';
+
+export const ErrorLayout = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  height: 100%;
+  gap: 3.2rem;
+  background-color: ${({ theme }) => theme.colors.lightGreen100};
+`;
+
+export const TitleContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 0.8rem;
+`;
+
+export const ErrorTitle = styled.h1`
+  ${({ theme }) => theme.fonts.heading22}
+`;
+
+export const ErrorSubTitle = styled.h2`
+  ${({ theme }) => theme.fonts.headline17}
+`;
+
+export const ErrorButton = styled.button`
+  ${({ theme }) => theme.fonts.body16};
+  background-color: ${({ theme }) => theme.colors.green600};
+  color: ${({ theme }) => theme.colors.white};
+
+  display: flex;
+  justify-content: center;
+  padding: 1.5rem 3rem;
+  gap: 0.4rem;
+  border-radius: 3.2rem;
+
+  width: 40%;
+`;
