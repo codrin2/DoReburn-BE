b4778bd5a9b6a57e51b9dec45a8e14c8d2ada4d8
Gyuhan Park <rbgks1937@naver.com>
2025-02-24 18:08:50 +0900

    refactor: 중복해서 사용하는 memberInfo 쿼리를 공통 훅으로 분리 #227

--- frontend/src/pages/MyPage/hooks/useMemberInfoQuery.ts
+++
@@ -1,8 +0,0 @@
-import { useQuery } from '@tanstack/react-query';
-
-import { getMemberInfo } from '@/api/member';
-import { QUERY_KEY } from '@/constants/queryKey';
-
-export const useMemberInfoQuery = () => {
-  return useQuery({ queryKey: [QUERY_KEY.memberInfo], queryFn: getMemberInfo });
-};
