eba0603d53499c838ef171d8d90bf1b6dae9a16b
son-young-jin <sdudwls128@naver.com>
2025-02-24 10:54:23 +0900

    fix: ios 환경 notification 에러 해결 #213

--- frontend/src/hooks/useNotificationPermission.ts
+++ frontend/src/hooks/useNotificationPermission.ts
@@ -3,9 +3,15 @@ import { useEffect, useState } from 'react';
 import { subscribePushNotification } from '@/api/notification';
 
 const useNotificationPermission = () => {
-  const [permission, setPermission] = useState<NotificationPermission>(Notification.permission);
+  const [permission, setPermission] = useState<NotificationPermission>(
+    typeof Notification !== 'undefined' ? Notification.permission : 'denied',
+  );
 
   useEffect(() => {
+    if (typeof Notification === 'undefined') {
+      return;
+    }
+
     if (permission === 'default') {
       alert('이동 시간이 끝나기 전에 알림으로 리마인드를 받아보세요! 🚀\n 놓치지 않게 도와드려요.');
 
