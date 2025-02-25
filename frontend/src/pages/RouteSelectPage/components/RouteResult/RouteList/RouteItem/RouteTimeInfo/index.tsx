afbabcb95615169b723ecd70651ec48ca7b4cc6b
son-young-jin <sdudwls128@naver.com>
2025-02-25 11:51:16 +0900

    fix: 분 단위를 시간 단위로 변경하는 유틸 함수 추가 #239

--- frontend/src/pages/RouteSelectPage/components/RouteResult/RouteList/RouteItem/RouteTimeInfo/index.tsx
+++ frontend/src/pages/RouteSelectPage/components/RouteResult/RouteList/RouteItem/RouteTimeInfo/index.tsx
@@ -2,16 +2,7 @@ import * as S from './RouteTimeInfo.styled';
 
 import Icon from '@/components/Icon';
 import theme from '@/styles/theme';
-
-const formatTime = (time: number) => {
-  if (time < 60) {
-    return `${time}분`;
-  }
-  const hour = Math.floor(time / 60);
-  const minute = time % 60;
-
-  return `${hour}시간 ${minute > 0 ? `${minute}분` : ''}`;
-};
+import { minutesToHours } from '@/utils/time';
 
 interface RouteTimeInfoProps {
   totalTime: number;
@@ -21,12 +12,12 @@ interface RouteTimeInfoProps {
 const RouteTimeInfo = ({ totalTime, totalSectionTime }: RouteTimeInfoProps) => {
   return (
     <S.TimeContainer>
-      <S.TotalTime>{formatTime(totalTime)}</S.TotalTime>
+      <S.TotalTime>{minutesToHours(totalTime)}</S.TotalTime>
 
       <S.TotalSectionTimeBox>
         <Icon icon="Fire" width={16} height={16} color={theme.colors.green600} />
         <span>활용 가능 시간</span>
-        <S.TotalSectionTime>{formatTime(totalSectionTime)}</S.TotalSectionTime>
+        <S.TotalSectionTime>{minutesToHours(totalSectionTime)}</S.TotalSectionTime>
       </S.TotalSectionTimeBox>
     </S.TimeContainer>
   );
