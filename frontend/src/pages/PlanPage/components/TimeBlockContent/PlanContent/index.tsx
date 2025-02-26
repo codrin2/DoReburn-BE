c2b85cae52e715791edda7d2ec40af8e7504ced1
Gyuhan Park <rbgks1937@naver.com>
2025-02-27 00:49:17 +0900

    refactor: 버블링 이벤트 전파 막기 #247

--- frontend/src/pages/PlanPage/components/TimeBlockContent/PlanContent/index.tsx
+++ frontend/src/pages/PlanPage/components/TimeBlockContent/PlanContent/index.tsx
@@ -72,6 +72,8 @@ const PlanContent = ({ paths }: PlanContentProps) => {
   };
 
   const handleTouchEnd = async (e: React.TouchEvent<HTMLElement>) => {
+    e.stopPropagation();
+
     if (longPressTimeoutRef.current) {
       clearTimeout(longPressTimeoutRef.current);
       longPressTimeoutRef.current = null;
