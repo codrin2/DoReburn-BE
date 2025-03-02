4deabfb325e732e745434e0cc5e3dd71c09b3637
rbgksqkr <rbgks1937@naver.com>
2025-03-03 02:25:22 +0900

    chore: vite 설정 변경 #249

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -9,6 +9,7 @@ export default defineConfig(({ mode }) => ({
   plugins: [
     react(),
     VitePWA({
+      strategies: 'injectManifest',
       filename: 'firebase-messaging-sw.js',
       registerType: 'autoUpdate',
       devOptions: {
