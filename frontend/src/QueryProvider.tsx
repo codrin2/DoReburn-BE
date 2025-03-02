804b5ba1971703d4d521c0db3be2f4f5a1df649e
rbgksqkr <rbgks1937@naver.com>
2025-03-03 02:11:07 +0900

    refactor: 디버깅용 코드 제거 # #249

--- frontend/src/QueryProvider.tsx
+++ frontend/src/QueryProvider.tsx
@@ -1,4 +1,5 @@
 import { MutationCache, QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import { PropsWithChildren, useMemo } from 'react';
 
 import {
@@ -43,7 +44,12 @@ const QueryProvider = ({ children }: PropsWithChildren) => {
     [toast],
   );
 
-  return <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>;
+  return (
+    <QueryClientProvider client={queryClient}>
+      {children}
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
+  );
 };
 
 export default QueryProvider;
