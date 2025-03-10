e19b52d07699032dd4507411d586aa4d2ebbbb29
codrin2 <hssang127@gmail.com>
2025-03-11 04:53:35 +0900

    refactor: RestClientConfig core 패키지로 이동 #254

--- backend/src/main/java/com/dubu/backend/notification/core/RestClientConfig.java
+++ backend/src/main/java/com/dubu/backend/notification/core/RestClientConfig.java
@@ -1,4 +1,4 @@
-package com.dubu.backend.notification.infrastructure;
+package com.dubu.backend.notification.core;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
