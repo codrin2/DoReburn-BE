5e6918863c52311e199e62ebccf70af845f4e143
khw7385 <khw7385@naver.com>
2025-04-15 15:34:05 +0900

    feat: Plan 종료 이벤트 핸들러 정의 #253

---
+++ backend/src/main/java/com/dubu/backend/plan/infrastructure/PlanEventHandler.java
@@ -0,0 +1,33 @@
+package com.dubu.backend.plan.infrastructure;
+
+import com.dubu.backend.plan.core.exception.PlanNotFoundException;
+import com.dubu.backend.plan.domain.Plan;
+import com.dubu.backend.plan.domain.repository.PlanRepository;
+import com.dubu.backend.plan.infrastructure.api.CellCategoryCommandApi;
+import com.dubu.backend.plan.application.event.PlanEndedEvent;
+import com.dubu.backend.todo.domain.Todo;
+import com.dubu.backend.todo.infra.repository.TodoRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.event.EventListener;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+@Component
+@RequiredArgsConstructor
+public class PlanEventHandler {
+    private final TodoRepository todoRepository;
+    private final PlanRepository planRepository;
+    private final CellCategoryCommandApi cellCategoryCommandApi;
+
+    @EventListener(PlanEndedEvent.class)
+    public void handlePlanEndedEvent(PlanEndedEvent event){
+        Plan beforePlan = planRepository.findTopByMemberIdAndIsCompletedAndIdNotOrderByCreatedAtDesc(event.memberId(), true, event.plan().getId())
+                .orElseThrow(PlanNotFoundException::new);
+
+        List<Todo> beforeTodos = todoRepository.findByPlanAndIsCompleted(beforePlan, true);
+        List<Todo> recentTodos = todoRepository.findByPlanAndIsCompleted(event.plan(), true);
+
+        cellCategoryCommandApi.updateCellCategory(event.memberId(), beforeTodos, recentTodos);
+    }
+}
