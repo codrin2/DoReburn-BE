ba35215cbc0e211914dd1e190ea7e4ce32b67e3a
khw7385 <khw7385@naver.com>
2025-04-15 15:33:36 +0900

    feat: TempMember 의 위치 수정 및 CellCategory 업데이트 관련 기능 응용 서비스에 추가 #253

--- backend/src/main/java/com/dubu/backend/member/application/MemberLocationFacade.java
+++ backend/src/main/java/com/dubu/backend/member/application/MemberLocationFacade.java
@@ -1,10 +1,30 @@
 package com.dubu.backend.member.application;
 
-import com.dubu.backend.member.domain.MemberLocation;
+
+import ch.hsr.geohash.GeoHash;
+import com.dubu.backend.member.api.request.CellCategoryUpdateByLocationRequest;
+import com.dubu.backend.member.api.request.CellCategoryUpdateByPlanRequest;
+import com.dubu.backend.member.application.api.PlanQueryApi;
+import com.dubu.backend.member.core.exception.MemberNotFoundException;
+import com.dubu.backend.member.domain.model.Category;
+import com.dubu.backend.member.domain.model.MemberLocation;
+import com.dubu.backend.member.domain.model.TempMember;
+import com.dubu.backend.member.application.event.MemberLocationUpdatedEvent;
 import com.dubu.backend.member.domain.repository.MemberLocationRepository;
 import com.dubu.backend.member.domain.repository.MemberRepository;
+import com.dubu.backend.member.domain.repository.TempMemberRepository;
+
+import com.dubu.backend.member.domain.service.CellCategoryManagementService;
 import lombok.RequiredArgsConstructor;
+import org.locationtech.jts.geom.Coordinate;
+import org.locationtech.jts.geom.GeometryFactory;
+import org.locationtech.jts.geom.Point;
+import org.locationtech.jts.geom.PrecisionModel;
+import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 import static com.dubu.backend.member.application.MemberServiceHelper.findExistingMember;
 
@@ -13,9 +33,54 @@ import static com.dubu.backend.member.application.MemberServiceHelper.findExisti
 public class MemberLocationFacade {
     private final MemberRepository memberRepository;
     private final MemberLocationRepository memberLocationRepository;
+    private final TempMemberRepository tempMemberRepository;
+    private final PlanQueryApi planQueryApi;
+
+    private final GeometryFactory geometryFactory = new GeometryFactory(new PrecisionModel(), 4326);
+    private final ApplicationEventPublisher eventPublisher;
+    private final CellCategoryManagementService cellCategoryManagementService;
 
     public void updateMemberLocation(Long memberId, MemberLocation location) {
         findExistingMember(memberRepository, memberId);
         memberLocationRepository.saveMemberLocation(memberId, location);
+    }
+
+    @Transactional
+    public void updateTempMemberLocation(Long memberId, MemberLocation location){
+        TempMember member = tempMemberRepository.findById(memberId)
+                .orElseThrow(() -> new MemberNotFoundException(memberId));
+
+        GeoHash oldGeoHash = null;
+        if(member.hasLocation()){
+            Point oldLocation = member.getLocation();
+            oldGeoHash = GeoHash.withCharacterPrecision(oldLocation.getY(), oldLocation.getX(), 7);
+        }
+
+        Point newLocation = geometryFactory.createPoint(new Coordinate(location.x_coordinate(), location.y_coordinate()));
+        GeoHash newGeoHash = GeoHash.withCharacterPrecision(newLocation.getY(), newLocation.getX(), 7);
+
+        if(oldGeoHash == null || !oldGeoHash.equals(newGeoHash)){
+            eventPublisher.publishEvent(new MemberLocationUpdatedEvent(memberId, oldGeoHash, newGeoHash));
+        }
+
+        member.updateLocation(newLocation);
+    }
+
+    @Transactional
+    public void updateCellCategoryByLocationChange(Long memberId, CellCategoryUpdateByLocationRequest request){
+        TempMember member = tempMemberRepository.findById(memberId)
+                .orElseThrow(() -> new MemberNotFoundException(memberId));
+
+        List<Category> categories = planQueryApi.getRecentPlanTodoCategories(member);
+
+        cellCategoryManagementService.updateCellCategoryByLocationChange(request.oldGeoHash(), request.newGeoHash(), categories);
+    }
+
+    @Transactional
+    public void updateCellCategoryByPlanChange(Long memberId, CellCategoryUpdateByPlanRequest request){
+        TempMember member = tempMemberRepository.findById(memberId)
+                .orElseThrow(() -> new MemberNotFoundException(memberId));
+
+        cellCategoryManagementService.updateCellCategoryByPlanChange(member.getLocation(), request.beforeCategoryIds(), request.recentCategoryIds());
     }
 }
