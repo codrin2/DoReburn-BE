7c4372dc408ae9eac043eeaac5343474463c4f57
khw7385 <50816902+khw7385@users.noreply.github.com>
2025-04-16 14:08:16 +0900

    fix: propagation import 문 추가

--- backend/src/main/java/com/dubu/backend/member/application/MemberLocationFacade.java
+++ backend/src/main/java/com/dubu/backend/member/application/MemberLocationFacade.java
@@ -23,6 +23,7 @@ import org.locationtech.jts.geom.PrecisionModel;
 import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.transaction.annotation.Propagation;
 
 import java.util.List;
 
