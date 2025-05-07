80e8c631d87ca54d33c0749097b8bdc11914aeae
khw7385 <khw7385@naver.com>
2025-05-07 21:32:32 +0900

    fix: api 응답 dto 객체 생성 로직 추가 #264

--- backend/src/main/java/com/dubu/backend/core/domain/SuccessResponse.java
+++ backend/src/main/java/com/dubu/backend/core/domain/SuccessResponse.java
@@ -3,4 +3,7 @@ package com.dubu.backend.core.domain;
 public record SuccessResponse<D>(
         D data
 ) {
+    public static <D> SuccessResponse<D> of(D data){
+        return new SuccessResponse<D>(data);
+    }
 }
