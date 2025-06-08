ba602c091eb333443ae1665d44c720c288e3cf5a
khw7385 <khw7385@naver.com>
2025-06-09 00:55:19 +0900

    chore: 오류 해결을 위한 로그 설정

--- backend/src/main/java/com/dubu/backend/plan/core/RestClientConfig.java
+++ backend/src/main/java/com/dubu/backend/plan/core/RestClientConfig.java
@@ -1,17 +1,21 @@
 package com.dubu.backend.plan.core;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestClient;
 
 @Configuration("plan.restClientConfig")
+@Slf4j
 public class RestClientConfig {
     @Value("${api.local-url}")
     private String LOCAL_URL;
 
     @Bean("plan.restClient")
     public RestClient restClient(){
+        log.info("Local URL: {}", LOCAL_URL);
+
         return RestClient.builder()
                 .baseUrl(LOCAL_URL)
                 .build();
