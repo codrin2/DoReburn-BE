3cd83378f04fb57b63a46bfa577c55318ea8b297
khw7385 <khw7385@naver.com>
2025-06-08 15:43:10 +0900

    feat(TodoRepository): Todo 컨텍스트 Todo Repository 조회 메서드 추가 #275

--- backend/src/main/java/com/dubu/backend/todo/domain/repository/TodoRepository.java
+++ backend/src/main/java/com/dubu/backend/todo/domain/repository/TodoRepository.java
@@ -1,11 +1,12 @@
 package com.dubu.backend.todo.domain.repository;
 
-import com.dubu.backend.todo.domain.Category;
 import com.dubu.backend.todo.domain.Todo;
 import com.dubu.backend.todo.domain.enums.TodoType;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface TodoRepository extends JpaRepository<Todo, Long>, TodoQueryRepository {
     long countByScheduleId(Long scheduleId);
@@ -15,10 +16,16 @@ public interface TodoRepository extends JpaRepository<Todo, Long>, TodoQueryRepo
     boolean existsByMemberIdAndParentInfoParentIdAndType(Long memberId, Long parentId, TodoType type);
     boolean existsBySubPathIdAndParentInfoParentId(Long subPathId, Long parentTodoId);
 
+    Optional<Todo> findByMemberIdAndParentInfoParentId(Long memberId, Long parentId);
+
     List<Todo> findByIdIn(List<Long> ids);
     List<Todo> findByScheduleId(Long scheduleId);
     List<Todo> findBySubPathId(Long subPathId);
-    List<Todo> findByCategoryInAndType(List<Category> categories, TodoType type);
 
+    @Query("SELECT t FROM Todo t JOIN FETCH t.category WHERE t.subPathId in :subPathIds")
+    List<Todo> findBySubPathIdIn(List<Long> subPathIds);
+
+    @Query("SELECT t FROM Todo t JOIN FETCH t.category WHERE t.subPathId in :subPathIds AND t.isCompleted = :isCompleted")
+    List<Todo> findBySubPathIdInAndIsCompleted(List<Long> subPathIds, boolean isCompleted);
 
 }
