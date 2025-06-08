3c8f2fe1c131b15ecc9daddaed097c5ce82382a6
khw7385 <khw7385@naver.com>
2025-06-08 19:58:57 +0900

    feat: 경로 시간 별 Todo 난이도 기준 추가 #277

--- backend/src/main/java/com/dubu/backend/todo/domain/policy/TodoDifficultyBasedPathTimePolicy.java
+++ backend/src/main/java/com/dubu/backend/todo/domain/policy/TodoDifficultyBasedPathTimePolicy.java
@@ -13,6 +13,7 @@ public class TodoDifficultyBasedPathTimePolicy {
     public static List<TodoDifficulty> determineDifficulty(int pathTime){
         if(pathTime < 10) return List.of(EASY);
         else if(pathTime < 20) return List.of(EASY, NORMAL);
-        else return List.of(NORMAL, HARD);
+        else if(pathTime < 30) return List.of(NORMAL, HARD);
+        else return List.of(HARD);
     }
 }
