78887837828648f71fa1225d824df52f7bf55887
khw7385 <khw7385@naver.com>
2025-06-08 15:24:57 +0900

    refactor: 통계 관련 리팩토링 #275 도메인 모델 변경으로 인한 수정

--- backend/src/main/java/com/dubu/backend/plan/domain/vo/WeeklyTodoStats.java
+++ backend/src/main/java/com/dubu/backend/plan/domain/vo/WeeklyTodoStats.java
@@ -1,6 +1,5 @@
 package com.dubu.backend.plan.domain.vo;
 
-import com.dubu.backend.plan.domain.Category;
 import lombok.Builder;
 
 import java.time.Duration;
@@ -14,14 +13,14 @@ public record WeeklyTodoStats(
         int totalTodoCount,
         Duration totalUsageTime,
         Duration lastWeekDiff,
-        Map<Category, TodoStats> categoryTodoStats
+        Map<String, TodoStats> categoryTodoStats
 ) {
     public static WeeklyTodoStats of(Map<LocalDate, Duration> dayUsageTimeMap,
                                      int totalTodoCount,
                                      Duration totalUsageTime,
                                      Duration lastWeekDiff,
-                                     Map<Category, Integer> categoryCountMap,
-                                     Map<Category, Duration> categoryUsageTimeMap){
+                                     Map<String, Integer> categoryCountMap,
+                                     Map<String, Duration> categoryUsageTimeMap){
         return WeeklyTodoStats.builder()
                 .dayUsageTimeMap(Map.copyOf(dayUsageTimeMap))
                 .totalTodoCount(totalTodoCount)
