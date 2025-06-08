03488b56cf6ab5234f2a933403ac9b319eb9d030
khw7385 <khw7385@naver.com>
2025-06-08 15:02:21 +0900

    refactor: Plan 컨텍스트 repository 수정 #275

--- backend/src/main/java/com/dubu/backend/plan/domain/repository/CustomPlanRepository.java
+++ backend/src/main/java/com/dubu/backend/plan/domain/repository/CustomPlanRepository.java
@@ -1,12 +1,11 @@
 package com.dubu.backend.plan.domain.repository;
 
-import com.dubu.backend.plan.domain.Member;
 import com.dubu.backend.plan.domain.Plan;
 import com.dubu.backend.plan.domain.repository.dto.PlanSearchCond;
 
-import java.time.LocalDateTime;
 import java.util.List;
 
 public interface CustomPlanRepository {
-    List<Plan> findPlans(Member member, PlanSearchCond cond);
+    List<Plan> findPlans(PlanSearchCond cond);
+    List<Long> findCompletedRecentPlansId(List<Long> memberIds);
 }
--- backend/src/main/java/com/dubu/backend/plan/domain/repository/PlanRepository.java
+++ backend/src/main/java/com/dubu/backend/plan/domain/repository/PlanRepository.java
@@ -1,30 +1,15 @@
 package com.dubu.backend.plan.domain.repository;
 
-import com.dubu.backend.member.domain.model.Member;
 import com.dubu.backend.plan.domain.Plan;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
-import java.time.LocalDateTime;
-import java.util.List;
 import java.util.Optional;
 
 public interface PlanRepository extends JpaRepository<Plan, Long>, CustomPlanRepository {
-    @Query("SELECT p FROM Plan p LEFT JOIN FETCH p.feedback WHERE p.member.id = :memberId ORDER BY p.createdAt DESC LIMIT 1")
+    @Query("SELECT p FROM Plan p LEFT JOIN FETCH p.feedback WHERE p.memberId = :memberId ORDER BY p.createdAt DESC LIMIT 1")
     Optional<Plan> findTopByMemberIdOrderByCreatedAtDesc(Long memberId);
 
-    @Query("SELECT p FROM Plan p LEFT JOIN FETCH p.feedback WHERE p.member= :member AND p.isCompleted = :isCompleted ORDER BY p.createdAt DESC LIMIT 1")
-    Optional<Plan> findTopByMemberAndIsCompletedOrderByCreatedAtDesc(Member member, boolean isCompleted);
-
-    @Query("SELECT p FROM Plan p WHERE p.member.id = :memberId AND p.isCompleted = :isCompleted ORDER BY p.createdAt DESC LIMIT 1")
+    @Query("SELECT p FROM Plan p WHERE p.memberId = :memberId AND p.isCompleted = :isCompleted ORDER BY p.createdAt DESC LIMIT 1")
     Optional<Plan> findTopByMemberIdAndIsCompletedOrderByCreatedAtDesc(Long memberId, boolean isCompleted);
-
-    @Query("SELECT p FROM Plan p WHERE p.member.id = :memberId AND p.isCompleted = :isCompleted AND p.id <> :excludedId ORDER BY p.createdAt DESC LIMIT 1")
-    Optional<Plan> findTopByMemberIdAndIsCompletedAndIdNotOrderByCreatedAtDesc(Long memberId, boolean isCompleted, Long excludedId);
-
-    @Query("SELECT p FROM Plan p JOIN FETCH p.feedback WHERE p.member = :member AND p.createdAt between :startTime AND :endTime")
-    List<Plan> findByMemberAndCreatedAtBetween(Member member, LocalDateTime startTime, LocalDateTime endTime);
-
-    @Query("SELECT p FROM Plan p JOIN FETCH p.feedback JOIN FETCH p.subPaths WHERE p.member = :member AND p.isCompleted = true AND p.createdAt between :startTime AND :endTime")
-    List<Plan> findWithPathsByMemberAndCreatedAtBetween(Member member, LocalDateTime startTime, LocalDateTime endTime);
 }
