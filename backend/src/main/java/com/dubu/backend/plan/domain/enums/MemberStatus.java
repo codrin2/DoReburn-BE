f075c083b591167dadf8698586696b1ed6218fa8
khw7385 <khw7385@naver.com>
2025-06-08 14:48:54 +0900

    refactor: 기존 클래스 삭제 #275

--- backend/src/main/java/com/dubu/backend/plan/domain/enums/MemberStatus.java
+++
@@ -1,5 +0,0 @@
-package com.dubu.backend.plan.domain.enums;
-
-public enum MemberStatus {
-    ONBOARDING, STOP, MOVE, FEEDBACK;
-}
--- backend/src/main/java/com/dubu/backend/plan/domain/repository/MemberRepository.java
+++
@@ -1,7 +0,0 @@
-package com.dubu.backend.plan.domain.repository;
-
-import com.dubu.backend.plan.domain.Member;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface MemberRepository extends JpaRepository<Member, Long> {
-}
