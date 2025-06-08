27547ef46f2dbf2463e1c8e735f32225e4ed756e
khw7385 <khw7385@naver.com>
2025-06-08 15:56:50 +0900

    feat: 내부 통신을 위한 api의 경로 패턴 추가 #275

--- backend/src/main/java/com/dubu/backend/core/config/WebConfig.java
+++ backend/src/main/java/com/dubu/backend/core/config/WebConfig.java
@@ -39,6 +39,6 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
         registry.addInterceptor(tokenInterceptor)
-                .addPathPatterns("/members/**", "/plans/**", "/routes/**", "/todos/**", "/statistics/**", "/share/**");
+                .addPathPatterns("/members/**", "/plans/**", "/routes/**", "/todos/**", "/statistics/**", "/share/**", "/internal/**");
     }
 }
